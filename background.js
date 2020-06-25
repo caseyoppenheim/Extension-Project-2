cf4340c3ef57ce7a606d734a52ad302afdb518b7
Casey Oppenheim <casey.oppenheim@yahoo.com>
2020-06-25 11:14:43 -0400

    made some changes to method, still doesn't work properly but it does something now
    q

--- background.js
+++ background.js
@@ -1,24 +1,27 @@
 function onBeforeRequest(data){
     //perhaps put 'data' in parameter
     console.log("top of on before request method");
-    console.log(data);
+    console.log(data.initiator);
     //need to return something
     //this is where blocking goes and also figuring out whether something needs to be blocked
-    var urlNotFixed = window.location.toString();
+
+    //rework all of this!
+    var urlNotFixed = data.initiator;
     urlNotFixed.replace(" ", "");
     urlNotFixed.toLowerCase();
     var urlFixed = urlNotFixed;
-    console.log(urlFixed);
-    var blockedList = "facebook.com";
-    if(urlFixed.contains(blockedList)){
-        return webRequest.BlockingResponse;
+    if(urlFixed.includes(blockedList)){
+        console.log("inside if statement");
+        //okay so it's entering the if statement correctly but then nothing happens and the code continues to run
+        return {cancel : true};
     }
 
     //for block list: look at string mdn on google and look at the methods for the string object
     
 }
+var blockedList = "facebook.com";
 console.log("executing background code");
-chrome.webRequest.onBeforeRequest.addListener(onBeforeRequest);
+chrome.webRequest.onBeforeRequest.addListener(onBeforeRequest, {urls: ["http://*/*","https://*/*"] }, ["blocking"]);
 //you can remove 'blocking' at the end so we can make sure that chrome is running the method just to test
 
 
